1d46af6d357e978deea7ecc9a946c11abdcfaeaa
Sahithi Ravipati <sahithi.ravipati@yahoo.com>
2023-04-14 21:41:53 -0500

    1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(<
+    div>
+      <Head>
+      <meta name="google-site-verification" content="kPttY16UyrOFgjS9OPsUiju9CekHHEPaWFAZ5hg6ngE" />
+      </Head>
+      <Component {...pageProps} />
+    </div>);
 }
